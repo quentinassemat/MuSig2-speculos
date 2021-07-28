c62f09b4e9c6ee77e980eb4b653c9fc1476ef05b
yhql <victor_servant@hotmail.com>
2021-07-28 20:35:11 +0200

    cx_bn_ wrappers

---
+++ rust4speculos/src/cx_helpers.rs
@@ -0,0 +1,110 @@
+use nanos_sdk::io::SyscallError;
+use nanos_sdk::bindings;
+
+#[derive(Clone, Copy)]
+pub struct CxBn {
+	pub x: u32
+}
+
+impl CxBn {
+	pub fn new() -> CxBn {
+		CxBn { x: 0 }
+	}
+}
+
+pub fn cx_bn_lock(word_nbytes: u32, flags: u32) -> Result<(), SyscallError> {
+    let err = unsafe { bindings::cx_bn_lock(word_nbytes as u32, flags) };
+    if err != 0 {
+        Err(err.into())
+    } else {
+        Ok(())
+    }
+}
+
+pub fn cx_bn_unlock() -> Result<(), SyscallError> {
+    let err = unsafe { bindings::cx_bn_unlock() };
+    if err != 0 {
+        Err(err.into())
+    } else {
+        Ok(())
+    }
+}
+
+pub fn cx_bn_is_locked() -> bool {
+    unsafe { bindings::cx_bn_is_locked() }
+}
+
+pub fn cx_bn_alloc(nbytes: u32) -> Result<CxBn, SyscallError> {
+	let mut x = CxBn::new();
+    let err = unsafe { bindings::cx_bn_alloc(&mut x.x, nbytes) };
+	if err != 0 {
+		Err(err.into())
+	} else {
+		Ok(x)
+	}  
+}
+
+pub fn cx_bn_alloc_init(nbytes: u32, value: &[u8]) -> Result<CxBn, SyscallError> {
+	let mut x = CxBn::new();
+    let err = unsafe { bindings::cx_bn_alloc_init(&mut x.x, nbytes, value.as_ptr(), value.len() as u32) };
+	if err != 0 {
+		Err(err.into())
+	} else {
+		Ok(x)
+	}  
+}
+
+pub fn cx_bn_export(x: CxBn, bytes: &mut [u8]) -> Result<(), SyscallError> {
+    let err = unsafe { bindings::cx_bn_export(x.x, bytes.as_mut_ptr(), bytes.len() as u32) };
+	if err != 0 {
+		Err(err.into())
+	} else {
+		Ok(())
+	}  
+}
+
+pub fn cx_bn_mod_add(r: CxBn, a: CxBn, b: CxBn, n: CxBn) -> Result<(), SyscallError> {
+    let err = unsafe { bindings::cx_bn_mod_add(r.x, a.x, b.x, n.x) };
+	if err != 0 {
+		Err(err.into())
+	} else {
+		Ok(())
+	}  
+}
+
+pub fn cx_ecpoint_alloc(cv: bindings::cx_curve_t) -> Result<bindings::cx_ecpoint_t, SyscallError> {
+	let mut p = bindings::cx_ecpoint_t::default();
+    let err = unsafe { bindings::cx_ecpoint_alloc(&mut p, cv) };
+	if err != 0 {
+		Err(err.into())
+	} else {
+		Ok(p)
+	}  
+}
+
+pub fn cx_ecpoint_init(p: &mut bindings::cx_ecpoint_t, x: &[u8], y: &[u8]) -> Result<(), SyscallError> {
+    let err = unsafe { bindings::cx_ecpoint_init(p as *mut bindings::cx_ecpoint_t, x.as_ptr(), x.len() as u32, y.as_ptr(), y.len() as u32) };
+	if err != 0 {
+		Err(err.into())
+	} else {
+		Ok(())
+	}  
+}
+
+pub fn cx_ecpoint_add(r: &mut bindings::cx_ecpoint_t, p: &bindings::cx_ecpoint_t, q: &bindings::cx_ecpoint_t) -> Result<(), SyscallError> {
+    let err = unsafe { bindings::cx_ecpoint_add(r as *mut bindings::cx_ecpoint_t, p as *const bindings::cx_ecpoint_t, q as *const bindings::cx_ecpoint_t) };
+	if err != 0 {
+		Err(err.into())
+	} else {
+		Ok(())
+	}  
+}
+
+pub fn cx_ecpoint_export(p: &bindings::cx_ecpoint_t, x: &mut [u8], y: &mut [u8]) -> Result<(), SyscallError> {
+    let err = unsafe { bindings::cx_ecpoint_export(p, x.as_mut_ptr(), x.len() as u32, y.as_mut_ptr(), y.len() as u32) };
+	if err != 0 {
+		Err(err.into())
+	} else {
+		Ok(())
+	}  
+}
--- rust4speculos/src/data_types.rs
+++ rust4speculos/src/data_types.rs
@@ -1,9 +1,7 @@
 // FICHIER QUI GÈRE L'IMPLÉMENTATION DES DIFFÉRENTS TYPES DE DONNÉES POUR WRAPPER LES UNSAFE DU C
-mod self::utils;
-
+use crate::cx_helpers::*;
+use crate::utils;
 use core::str::from_utf8;
-use nanos_sdk::bindings;
-use nanos_sdk::ecc;
 use nanos_sdk::io::SyscallError;
 use nanos_ui::ui;
 
@@ -19,124 +17,70 @@ pub const M: &str = "Alice donne 1 Bitcoin à Bob";
 // TOUT S'UTILISE UNIQUEMENT EN LOCK
 
 pub struct Field {
-    pub index: u32,
+    pub index: CxBn,
     pub bytes: [u8 ; N_BYTES as usize],
-    pub ptr: *mut u8,
 }
 
 impl Field {
     pub fn new() -> Result<Field, SyscallError> { // new avec init à 0
         // déclaration
-        let index = 0_u32;
-        let mut bytes: [u8 ; N_BYTES as usize] = [0 ; N_BYTES as usize];
-        let ptr: *mut u8 = bytes.as_mut_ptr();
+        let bytes = [0u8; N_BYTES as usize];
 
         // on check si c'est lock
-        match bindings::cx_bn_locked() {
-            bindings::CX_OK => (),
-            bindings::CX_NOT_LOCKED => return Err(SyscallError::InvalidState),
-            _ => return Err(SyscallError::Unspecified),
+        if !cx_bn_is_locked() {
+            return Err(SyscallError::InvalidState);
         }
 
         // on alloue la mémoire
-        match bindings::cx_bn_alloc_init(&mut index, N_BYTES, ptr, N_BYTES) {
-            bindings::CX_OK => (),
-            bindings::CX_MEMORY_FULL => return Err(SyscallError::Overflow),
-            bindings::CX_INVALID_PARAMETER_SIZE => return Err(SyscallError::InvalidParameter),
-            _ => return Err(SyscallError::Unspecified),
-        }
+        let index = cx_bn_alloc_init(N_BYTES, &bytes)?;
 
         // on renvoie
-        Ok(Field {index, bytes, ptr})
+        Ok(Field {index, bytes})
     }
 
-    pub fn new_init(init: *const u8) -> Result<Field, SyscallError> { // init avec un pointeur vers un tableau de [u8 : N_BYTES = 32]
-        // déclaration
-        let index = 0_u32;
-        let mut bytes: [u8 ; N_BYTES as usize] = [0 ; N_BYTES as usize];
-        let ptr: *mut u8 = bytes.as_mut_ptr();
+    pub fn new_init(init: &[u8]) -> Result<Field, SyscallError> { // init avec un pointeur vers un tableau de [u8 : N_BYTES = 32]
 
         // on check si c'est lock
-        match bindings::cx_bn_locked() {
-            bindings::CX_OK => (),
-            bindings::CX_NOT_LOCKED => return Err(SyscallError::InvalidState),
-            _ => return Err(SyscallError::Unspecified),
+        if !cx_bn_is_locked() {
+            return Err(SyscallError::InvalidState);
         }
 
         // on alloue la mémoire
-        match bindings::cx_bn_alloc_init(&mut index, N_BYTES, init, N_BYTES) {
-            bindings::CX_OK => (),
-            bindings::CX_MEMORY_FULL => return Err(SyscallError::Overflow),
-            bindings::CX_INVALID_PARAMETER_SIZE => return Err(SyscallError::InvalidParameter),
-            _ => return Err(SyscallError::Unspecified),
-        }
+        let index = cx_bn_alloc_init(N_BYTES, &init)?;
 
         // on stocke en bytes
-        match bindings::cx_bn_export(index, ptr, N_BYTES) {
-            bindings::CX_OK => (),
-            bindings::CX_INVALID_PARAMETER_VALUE => return Err(SyscallError::InvalidParameter),
-            bindings::CX_INVALID_PARAMETER_SIZE => return Err(SyscallError::InvalidParameter),
-            bindings::CX_INVALID_PARAMETER => return Err(SyscallError::InvalidParameter),
-            bindings::CX_MEMORY_FULL => return Err(SyscallError::Overflow),
-            _ => return Err(SyscallError::Unspecified),
-        }
+        let mut bytes = [0u8; N_BYTES as usize];
+        cx_bn_export(index, &mut bytes)?;
 
         // on renvoie
-        Ok(Field {index, bytes, ptr})
+        Ok(Field {index, bytes})
     }
 
     pub fn add(&self, other: Field, modulo: Field) -> Result<Field, SyscallError> { // addition de self et other avec le modulo ( qui sera l'ordre de la courbe SECP256K1)
         // déclaration
-        let mut index = 0_u32;
-        let mut bytes: [u8 ; N_BYTES as usize] = [0 ; N_BYTES as usize];
-        let ptr: *mut u8 = bytes.as_mut_ptr();
+        let mut bytes = [0u8; N_BYTES as usize];
         
         // on check si c'est lock
-        match bindings::cx_bn_locked() {
-            bindings::CX_OK => (),
-            bindings::CX_NOT_LOCKED => return Err(SyscallError::InvalidState),
-            _ => return Err(SyscallError::Unspecified),
+        if !cx_bn_is_locked() {
+            return Err(SyscallError::InvalidState);
         }
 
         // on alloue la mémoire pour le resultat
-        unsafe {
-            match bindings::cx_bn_alloc(&mut index, N_BYTES) {
-                bindings::CX_OK => (),
-                bindings::CX_MEMORY_FULL => return Err(SyscallError::Overflow),
-                bindings::CX_INVALID_PARAMETER_SIZE => {
-                    return Err(SyscallError::InvalidParameter)
-                }
-                _ => return Err(SyscallError::Unspecified),
-            }
-        }
+        let index = cx_bn_alloc(N_BYTES)?;
 
         // on fait l'addition
-        unsafe {
-            match bindings::cx_bn_mod_add(index, self.index, other.index, modulo.index) {
-                bindings::CX_OK => (),
-                bindings::CX_NOT_LOCKED => return Err(SyscallError::InvalidState),
-                bindings::CX_INVALID_PARAMETER => return Err(SyscallError::InvalidParameter),
-                _ => return Err(SyscallError::Unspecified),
-            }
-        }
+        cx_bn_mod_add(index, self.index, other.index, modulo.index)?; 
 
         // on stocke le binaire
-        match bindings::cx_bn_export(index, sum_bytes_ptr, N_BYTES) {
-            bindings::CX_OK => (),
-            bindings::CX_INVALID_PARAMETER_VALUE => return Err(SyscallError::InvalidParameter),
-            bindings::CX_INVALID_PARAMETER_SIZE => return Err(SyscallError::InvalidParameter),
-            bindings::CX_INVALID_PARAMETER => return Err(SyscallError::InvalidParameter),
-            bindings::CX_MEMORY_FULL => return Err(SyscallError::Overflow),
-            _ => return Err(SyscallError::Unspecified),
-        }
+        cx_bn_export(index, &mut bytes)?;
 
         // on renvoie le résultat
-        Ok(Field {index, bytes, ptr})
+        Ok(Field {index, bytes})
     }
 
     pub fn show(&self) {
-        let hex = utils::to_hex(&self.bytes).map_err(|_| SyscallError::Overflow)?;
-        let m = from_utf8(&hex).map_err(|_| SyscallError::InvalidParameter)?;
+        let hex = utils::to_hex(&self.bytes).map_err(|_| SyscallError::Overflow).unwrap();
+        let m = from_utf8(&hex).map_err(|_| SyscallError::InvalidParameter).unwrap();
         ui::MessageScroller::new(m).event_loop();
     }
 }
